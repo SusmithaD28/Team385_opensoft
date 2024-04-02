be4c473b90a8534047834a384d8983291cbcec5d
SusmithaD28 <susmithadasavaraneni.28@gmail.com>
2024-04-02 21:04:58 +0530

    Deleted_Folders

--- Backend/auth/config.js
+++
@@ -1,3 +0,0 @@
-const JWT_SECRET="hello";
-const saltRounds = 10;
-module.exports={JWT_SECRET,saltRounds};
--- Backend/auth/middleware/multeruploads.js
+++
@@ -1,5 +0,0 @@
-const multer = require('multer');
-const storage = multer.memoryStorage();
-const multerUploads = multer({ storage }).single('videos');
-
-module.exports = multerUploads;
